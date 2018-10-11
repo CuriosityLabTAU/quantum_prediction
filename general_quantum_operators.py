8d89953252a4a944572e2a9b17e47e3303bcd9dc
Torr <torrpolakow@gmail.com>
2018-10-11 20:19:23 +0300

    hamiltonian

--- general_quantum_operators.py
+++ general_quantum_operators.py
@@ -81,4 +81,44 @@ def compose_H(full_h, all_q, n_qubits=4):
     # all_q = [q1, q2]
     H_ = zero_H(n_qubits)
 
+    Hmix_ = np.array([[5, 5], [5, -5]])
+
+    mix = np.zeros([4, 4])
+    mix[0, 0] = Hmix_[0, 0]
+    mix[0, -1] = Hmix_[0, 1]
+    mix[-1, 0] = Hmix_[1, 0]
+    mix[-1, -1] = Hmix_[1, 1]
+
+    so = np.kron(np.kron(mix, np.eye(2)), np.eye(2))
+    o = reorganize_operator([0, 3, 1, 2], so)
+
     return H_
+
+
+def reorganize_operator(qubits_order, operator_mat):
+    '''Reorganizing matrix from specific order to the right order [1,2,3,4].'''
+    N = len(qubits_order)
+    psi = create_Nqubit_state(N)
+    rho_scramb = rho_mat(psi)
+    rho_org = np.copy(rho_scramb)
+    nps, nq, nr, nc = rho_scramb.shape
+    re_rho = np.zeros([nr, nc])
+    # Scrambling rho according to given qubits order
+    for i in range(0, nr):
+        temp = np.zeros(nq)
+        for j in range(0, nc):
+            for k in range(0, nps):
+                for l in range(0, nq):
+                    # finding the index of the cell from the scrambled rho in the organized rho.
+                    temp[qubits_order[l]] = rho_scramb[k, l, i, j]
+                rho_scramb[k, :, i, j] = temp
+
+    # Reorganizing Rho matrix with the real values (Not just indices).
+    for i in range(0, nr):
+        for j in range(0, nc):
+            for k in range(0, nr):
+                for l in range(0, nc):
+                    if np.sum(rho_org[0, :, k, l] == rho_scramb[0, :, i, j]) == nq & np.sum(
+                                    rho_org[1, :, k, l] == rho_scramb[1, :, i, j]) == nq:
+                        re_rho[k, l] = operator_mat[i, j]
+    return re_rho
